a7343fbf483ad819e52299adee8c793e012027f6
Awamura-Jo <gc22001@anabuki-net.ne.jp>
2022-10-24 12:00:11 +0900

    課題12

--- 3DLv1_2022_vs2019/GameProgramming/src/CMatrix.cpp
+++ 3DLv1_2022_vs2019/GameProgramming/src/CMatrix.cpp
@@ -30,11 +30,15 @@ CMatrix CMatrix::Identity() {
 //Šg‘åk¬s—ñ‚Ìì¬
 //Scale(”{—¦X,”{—¦Y,”{—¦Zj
 CMatrix CMatrix::Scale(float sx, float sy, float sz) {
+	mM[0][0] = sx, mM[0][1] = 0, mM[0][2] = 0, mM[0][3] = 0;
+	mM[1][0] = 0, mM[1][1] = sy, mM[1][2] = 0, mM[1][3] = 0;
+	mM[2][0] = 0, mM[2][1] = 0, mM[2][2] = sz, mM[2][3] = 0;
+	mM[3][0] = 0, mM[3][1] = 0, mM[3][2] = 0, mM[3][3] = 1;
 	//‚±‚Ìs—ñ‚ğ•Ô‚·
 	return *this;
 }
 
-float CMatrix::M(int r, int c)const
+float CMatrix::M(int r, int c) const
 {
-	return ;
+	return mM[r][c];
 }
--- 3DLv1_2022_vs2019/GameProgramming/src/CVector.h
+++ 3DLv1_2022_vs2019/GameProgramming/src/CVector.h
@@ -1,7 +1,7 @@
 #pragma once
 #ifndef CVECTOR_H
 #define CVECTOR_H
-
+#include "CMatrix.h" //class CMatrix;でも可
 /*
 ベクトルクラス
 ベクトルデータを扱います
