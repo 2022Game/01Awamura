f97bd41833d461459b24f2e6c229261a4b3050db
Awamura-Jo <gc22001@anabuki-net.ne.jp>
2023-04-17 15:54:05 +0900

    kadai3

--- 3DLv2_2023_vs2019_00/3DLv2_2023_vs2019_00/GameProgramming/src/CModelX.h
+++ 3DLv2_2023_vs2019_00/3DLv2_2023_vs2019_00/GameProgramming/src/CModelX.h
@@ -5,22 +5,48 @@
 
 //領域解放をマクロ化
 #define SAFE_DELETE_ARRAY(a){if(a)delete[]a;a = nullptr;}
+
+#include <vector> //vectorクラスのインクルード（動的配列）
+#include"CMatrix.h" //マトリクスクラスのインクルード
+class CModelX; //CModelXクラスの宣言
+class CModelXFrame; //CModelXFrameクラスの宣言
+
+//CModelXFrameクラスの定義
+class CModelXFrame {
+	friend CModelX;
+public:
+	//コンストラクタ
+	CModelXFrame(CModelX* model);
+	//デストラクタ
+	~CModelXFrame();
+private:
+	std::vector<CModelXFrame*>mChild; //子フレームの配列
+	CMatrix mTransformMatrix; //変換行列
+	char* mpName; //フレーム名前
+	int mIndex; //フレーム番号
+};
+
 /*
 CModelX
 Xファイル形式の３Dモデルデータをプログラムで認識する
 */
 class CModelX {
+	friend CModelXFrame;
 public:
+	~CModelX();
+	//ノードの読み飛ばし
+	void SkipNode();
 	CModelX();
 	//ファイル読み込み
 	void Load(char* file);
 private:
-	char* mpPointer; //読み込み位置
-	char mToken[1024]; //取り出した単語の領域
+	std::vector<CModelXFrame*>mFrame; //フレーム配列
 	//単語の取り出し
 	char* GetToken();
 	//cが区切り文字ならtrueを返す
 	bool IsDelimiter(char c);
+	char* mpPointer; //読み込み位置
+	char mToken[1024]; //取り出した単語の領域
 };
 
 #endif
